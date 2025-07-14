a14b76aa4fd7a32b61441e7d37191a3e4ef2ca66
mooreApps22 <124527392+mooreApps22@users.noreply.github.com>
2025-07-14 16:03:49 +0100

    Almost working. Not quite right, but good progress.

--- ex01/sources/main.cpp
+++ ex01/sources/main.cpp
@@ -1,12 +1,50 @@
 #include <iostream>
 #include <stack>
+#include <vector>
 #include <sstream>
 #include <string>
 #include <cstdlib>
 #include "data.hpp"
 
-typedef std::stack<char>		Stack;
-typedef std::string::iterator	It;
+typedef std::stack<int, std::vector<int> >	Stack;
+typedef std::string::iterator				It;
+
+bool	isValidPush(char c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+bool	isArithmeticOperator(char c)
+{
+	return (c == '+' || c == '-' || c == '*' || c == '/');
+}
+
+int	runCalculation(int result, int num, char symbol)
+{
+	switch (symbol)
+	{
+		case '+':
+			return result + num;
+		case '-':
+			return result - num;
+		case '*':
+			return result * num;
+		case '/':
+		{
+			if (num == 0)
+			{
+				std::cerr << "Error " <<  symbol << std::endl;
+				return (0);
+			}
+			return result / num;
+		}
+		default:
+		{
+			std::cerr << "Error " <<  symbol << std::endl;
+			return (0);
+		}
+	}
+}
 
 int	main(int argc, char** argv)
 {
@@ -16,19 +54,38 @@ int	main(int argc, char** argv)
 	}
 
 	std::string		input(argv[1]);
-	Stack			stack;
+	Stack			numbers;
+	int				result = 0;
 
-	for (It it = input.begin(); it != input.end(); ++it) {
-		char	ch = *it;
-		stack.push(ch);
-		std::cout << "Load: " << ch << std::endl;
-	}
-
-	while (!stack.empty())
+	for (It it = input.begin(); it != input.end(); ++it)
 	{
-		char	ch = stack.top();
-		stack.pop();
-		std::cout << "Fire: " << ch << std::endl;
+		if (isValidPush(*it))
+		{
+			int num = *it - '0';
+			numbers.push(num);
+		// 	std::cout << "Load: " << *it << std::endl;
+		}
+		else if (isArithmeticOperator(*it))
+		{
+			while (!numbers.empty())
+			{	
+				int num = numbers.top();
+
+			//	std::cout << "Fire: " << num << std::endl;
+				result = runCalculation(result, num, *it);
+				numbers.pop();
+			}
+		}
+		else if ( *it == ' ')
+			continue ;
+		else
+		{
+			std::cerr << "Error" << std::endl;
+			break ;
+		}			
 	}
+
+	std::cout << result << std::endl;
+
 	return (0);
 }
